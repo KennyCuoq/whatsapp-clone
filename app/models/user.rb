730a4cf08813b31637f80789a65a32d870cc634a
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-11 03:47:34 +0100

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :messages
   validates :username, :image_url, :email, presence: true
-  validates :email, uniquness: true
+  validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
