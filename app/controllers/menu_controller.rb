e0c904676bf2f0af678312d136825c69c7b9a65b
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-13 01:03:56 +0100

    Add optional reference to user in chat table for users to iniate conversations with other users

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,5 +3,7 @@ class MenuController < ApplicationController
   skip_after_action :verify_authorized
 
   def user_dashboard
+    @user = current_user
+    # retrieve all chats where user
   end
 end
