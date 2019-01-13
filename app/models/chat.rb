d03149be3420615515b1ae4ba0c33b1b85cc40a4
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-13 04:13:41 +0100

    show relevant chat photo and username for each chat card in menu

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,7 +5,7 @@ class Chat < ApplicationRecord
   # This method takes one user argument and return the user they share the chat with
   def other_user(user)
     messages = self.messages.where("user_id != ?", user)
-    return messages[0].user.username
+    return messages[0].user
     # I FEEL THIS METHOD COULD BE REWRITTEN IN A MORE EFFICIENT AND SUGAR WAY
   end
 end
