5b80faa9cfdf1c769d1f93d7fae2706fd8f25857
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-14 01:41:14 +0100

    Update associations and seed to match new database structure

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,5 @@
 class Chat < ApplicationRecord
   has_many :messages
-  belongs_to :user
 
   # This method takes one user argument and return the user they share the chat with
   def other_user(user)
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :user
+  belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
   belongs_to :chat
-  validates :user_id, :chat_id, :content, presence: true
+  validates :chat_id, :content, :sender_id, :recipient_id, presence: true
 end
