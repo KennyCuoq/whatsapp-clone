92f8d2013e20a591e04cdf371fd2a15575270a91
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-15 18:49:16 +0100

    Refactoring

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,10 +3,7 @@ class Chat < ApplicationRecord
 
   # This method takes one user argument and return the user they share the chat with
   def other_user(user)
-    # messages = self.messages.where("user_id != ?", user)
-    # return messages[0].user
-    message = self.messages.first
-    message.sender == user ? message.recipient : message.sender
+    messages.first.sender == user ? message.recipient : message.sender
     # I FEEL THIS METHOD COULD BE REWRITTEN IN A MORE EFFICIENT AND SUGAR WAY
   end
 
