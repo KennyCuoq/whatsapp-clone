0d5a2fdd32b139f83350253b31d12215e652d750
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-15 19:08:44 +0100

    Chats show variable initialise

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,5 +2,7 @@ class ChatsController < ApplicationController
   def show
     @chat = Chat.find(params[:id])
     authorize @chat
+    @user = current_user
+    @contact = @chat.other_user(@user)
   end
 end
