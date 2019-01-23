9d664c5eb0021b83c55088bf5f510909ce756ebe
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-01-23 12:29:16 +0100

    User receives real time 'seen' receipts in CHats#Show

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -16,4 +16,22 @@ class UsersChannel < ApplicationCable::Channel
       chat_id: data['chat_id']
     }
   end
+
+  def messages_seen(data)
+    if data['type'] == 'messages seen'
+      contact_id = data['contact_id']
+      ActionCable.server.broadcast "user_#{contact_id}", {
+        type: data['type'],
+        user_id: data['user_id'],
+        contact_id: contact_id,
+        chat_id: data['chat_id']
+      }
+    elsif data['type'] == 'messages seen PATCH'
+      messages = Message.where("chat_id = ? AND recipient_id = ? AND seen = ?", data['chat_id'], data['user_id'], false)
+      messages.each do |message|
+        message.seen = true
+        message.save
+      end
+    end
+  end
 end
